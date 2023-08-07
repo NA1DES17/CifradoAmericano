e61ccc6e30e94aecde55803f42520134eb09fc0d
NA1DES17 <nosoyna1des17@gmail.com>
2023-08-07 20:05:59 -0300

    + Rachas - Colores fondo

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+// const cifradoAmericano = ["C", "D", "E", "F", "G", "A", "B"];
+// const notas = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"];
+
+// let spanIzq = document.getElementById("span-izq");
+// let spanDer = document.getElementById("span-der");
+
+// let btnSi = document.getElementById("si");
+// let btnNo = document.getElementById("no");
+
+// let rachaAciertos = 0;
+// let racha = document.getElementById("racha");
+// racha.textContent = rachaAciertos;
+
+// let numeroAleatorio = Math.floor(Math.random() * 7);
+// let numeroAleatorio2 = Math.floor(Math.random() * 7);
+// spanIzq.textContent = cifradoAmericano[numeroAleatorio];
+// spanDer.textContent = notas[numeroAleatorio2];
+
+// function cambiarValores() {
+//   numeroAleatorio = Math.floor(Math.random() * 7);
+//   numeroAleatorio2 = Math.floor(Math.random() * 7);
+
+//   spanIzq.textContent = cifradoAmericano[numeroAleatorio];
+//   spanDer.textContent = notas[numeroAleatorio2];
+
+//   return [numeroAleatorio, numeroAleatorio2];
+// }
+
+// btnSi.addEventListener("click", function () {
+//   let [numeroAleatorio, numeroAleatorio2] = cambiarValores();
+//   if (numeroAleatorio === numeroAleatorio2) {
+//     rachaAciertos++;
+//     racha.textContent = rachaAciertos;
+//     container.style.backgroundColor = "#23c483";
+//   } else {
+//     rachaAciertos = 0;
+//     racha.textContent = rachaAciertos;
+//     container.style.backgroundColor = "#c42e23";
+//   }
+// });
+
+// btnNo.addEventListener("click", function () {
+//   let [numeroAleatorio, numeroAleatorio2] = cambiarValores();
+//   if (numeroAleatorio !== numeroAleatorio2) {
+//     rachaAciertos++;
+//     racha.textContent = rachaAciertos;
+//     container.style.backgroundColor = "#23c483";
+//   } else {
+//     rachaAciertos = 0;
+//     racha.textContent = rachaAciertos;
+//     container.style.backgroundColor = "#c42e23";
+//   }
+// });
+//----------------
 const cifradoAmericano = ["C", "D", "E", "F", "G", "A", "B"];
 const notas = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"];
 
@@ -7,44 +61,47 @@ let spanDer = document.getElementById("span-der");
 let btnSi = document.getElementById("si");
 let btnNo = document.getElementById("no");
 
+let rachaAciertos = 0;
+let racha = document.getElementById("racha");
+racha.textContent = rachaAciertos;
+
 let numeroAleatorio = Math.floor(Math.random() * 7);
 let numeroAleatorio2 = Math.floor(Math.random() * 7);
-
 spanIzq.textContent = cifradoAmericano[numeroAleatorio];
 spanDer.textContent = notas[numeroAleatorio2];
 
-function correcto() {
-  container.style.backgroundImage = "none";
-  container.style.backgroundColor = "#23c483";
-  setTimeout(function () {
-    window.location.reload();
-  }, 500);
-}
+function cambiarValores() {
+  numeroAleatorio = Math.floor(Math.random() * 7);
+  numeroAleatorio2 = Math.floor(Math.random() * 7);
 
-function incorrecto() {
-  container.style.backgroundImage = "none";
-  container.style.backgroundColor = "#c42e23";
-  setTimeout(function () {
-    window.location.reload();
-  }, 500);
+  spanIzq.textContent = cifradoAmericano[numeroAleatorio];
+  spanDer.textContent = notas[numeroAleatorio2];
+
+  return [numeroAleatorio, numeroAleatorio2];
 }
 
 btnSi.addEventListener("click", function () {
+  let [numeroAleatorio, numeroAleatorio2] = cambiarValores();
   if (numeroAleatorio === numeroAleatorio2) {
+    rachaAciertos++;
+    racha.textContent = rachaAciertos;
     container.style.backgroundColor = "#23c483";
-    setInterval(correcto, 500);
   } else {
+    rachaAciertos = 0;
+    racha.textContent = rachaAciertos;
     container.style.backgroundColor = "#c42e23";
-    setInterval(incorrecto, 500);
   }
 });
 
 btnNo.addEventListener("click", function () {
+  let [numeroAleatorio, numeroAleatorio2] = cambiarValores();
   if (numeroAleatorio !== numeroAleatorio2) {
+    rachaAciertos++;
+    racha.textContent = rachaAciertos;
     container.style.backgroundColor = "#23c483";
-    setInterval(correcto, 500);
   } else {
+    rachaAciertos = 0;
+    racha.textContent = rachaAciertos;
     container.style.backgroundColor = "#c42e23";
-    setInterval(incorrecto, 500);
   }
 });

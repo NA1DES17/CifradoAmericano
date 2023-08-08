c9b95c93267ff96d421f49d7e8a6f049cc2ecc04
NA1DES17 <nosoyna1des17@gmail.com>
2023-08-08 15:04:22 -0300

    Faltan colores de acierto/error

--- script.js
+++ script.js
@@ -1,56 +1,3 @@
-// const cifradoAmericano = ["C", "D", "E", "F", "G", "A", "B"];
-// const notas = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"];
-
-// let spanIzq = document.getElementById("span-izq");
-// let spanDer = document.getElementById("span-der");
-
-// let btnSi = document.getElementById("si");
-// let btnNo = document.getElementById("no");
-
-// let rachaAciertos = 0;
-// let racha = document.getElementById("racha");
-// racha.textContent = rachaAciertos;
-
-// let numeroAleatorio = Math.floor(Math.random() * 7);
-// let numeroAleatorio2 = Math.floor(Math.random() * 7);
-// spanIzq.textContent = cifradoAmericano[numeroAleatorio];
-// spanDer.textContent = notas[numeroAleatorio2];
-
-// function cambiarValores() {
-//   numeroAleatorio = Math.floor(Math.random() * 7);
-//   numeroAleatorio2 = Math.floor(Math.random() * 7);
-
-//   spanIzq.textContent = cifradoAmericano[numeroAleatorio];
-//   spanDer.textContent = notas[numeroAleatorio2];
-
-//   return [numeroAleatorio, numeroAleatorio2];
-// }
-
-// btnSi.addEventListener("click", function () {
-//   let [numeroAleatorio, numeroAleatorio2] = cambiarValores();
-//   if (numeroAleatorio === numeroAleatorio2) {
-//     rachaAciertos++;
-//     racha.textContent = rachaAciertos;
-//     container.style.backgroundColor = "#23c483";
-//   } else {
-//     rachaAciertos = 0;
-//     racha.textContent = rachaAciertos;
-//     container.style.backgroundColor = "#c42e23";
-//   }
-// });
-
-// btnNo.addEventListener("click", function () {
-//   let [numeroAleatorio, numeroAleatorio2] = cambiarValores();
-//   if (numeroAleatorio !== numeroAleatorio2) {
-//     rachaAciertos++;
-//     racha.textContent = rachaAciertos;
-//     container.style.backgroundColor = "#23c483";
-//   } else {
-//     rachaAciertos = 0;
-//     racha.textContent = rachaAciertos;
-//     container.style.backgroundColor = "#c42e23";
-//   }
-// });
 //----------------
 const cifradoAmericano = ["C", "D", "E", "F", "G", "A", "B"];
 const notas = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"];
@@ -61,47 +8,59 @@ let spanDer = document.getElementById("span-der");
 let btnSi = document.getElementById("si");
 let btnNo = document.getElementById("no");
 
-let rachaAciertos = 0;
-let racha = document.getElementById("racha");
-racha.textContent = rachaAciertos;
+let numeroAleatorio;
+let numeroAleatorio2;
 
-let numeroAleatorio = Math.floor(Math.random() * 7);
-let numeroAleatorio2 = Math.floor(Math.random() * 7);
-spanIzq.textContent = cifradoAmericano[numeroAleatorio];
-spanDer.textContent = notas[numeroAleatorio2];
-
-function cambiarValores() {
+aleatorios();
+function aleatorios() {
   numeroAleatorio = Math.floor(Math.random() * 7);
   numeroAleatorio2 = Math.floor(Math.random() * 7);
-
   spanIzq.textContent = cifradoAmericano[numeroAleatorio];
   spanDer.textContent = notas[numeroAleatorio2];
-
-  return [numeroAleatorio, numeroAleatorio2];
 }
-
-btnSi.addEventListener("click", function () {
-  let [numeroAleatorio, numeroAleatorio2] = cambiarValores();
-  if (numeroAleatorio === numeroAleatorio2) {
-    rachaAciertos++;
-    racha.textContent = rachaAciertos;
-    container.style.backgroundColor = "#23c483";
-  } else {
-    rachaAciertos = 0;
-    racha.textContent = rachaAciertos;
-    container.style.backgroundColor = "#c42e23";
-  }
-});
+let spanRacha = document.getElementById("racha");
+let racha = 0;
+spanRacha.textContent = racha;
 
 btnNo.addEventListener("click", function () {
-  let [numeroAleatorio, numeroAleatorio2] = cambiarValores();
   if (numeroAleatorio !== numeroAleatorio2) {
-    rachaAciertos++;
-    racha.textContent = rachaAciertos;
-    container.style.backgroundColor = "#23c483";
+    racha++;
+    spanRacha.textContent = racha;
+    console.log(numeroAleatorio + " - " + numeroAleatorio2);
+    console.log(racha);
+    aleatorios();
   } else {
-    rachaAciertos = 0;
-    racha.textContent = rachaAciertos;
-    container.style.backgroundColor = "#c42e23";
+    console.log("La racha terminó");
+    racha = 0;
+    spanRacha.textContent = racha;
+    aleatorios();
   }
 });
+btnSi.addEventListener("click", function () {
+  if (numeroAleatorio === numeroAleatorio2) {
+    racha++;
+    spanRacha.textContent = racha;
+    console.log(numeroAleatorio + " - " + numeroAleatorio2);
+    console.log(racha);
+    aleatorios();
+  } else {
+    console.log("La racha terminó");
+    racha = 0;
+    spanRacha.textContent = racha;
+    aleatorios();
+  }
+});
+
+//------------------
+// let spanTest = document.getElementById("span-test");
+// let btnTest = document.getElementById("btn-test");
+
+// let n1 = Math.floor(Math.random() * 7);
+// let n2 = Math.floor(Math.random() * 7);
+// spanTest.textContent = n1;
+// //spanDer.textContent = notas[n2];
+
+// btnTest.addEventListener("click", function () {
+//   n1 = Math.floor(Math.random() * 7);
+//   spanTest.textContent = n1;
+// });
